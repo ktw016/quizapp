ea0cd34a91387315ae1ad1ddaf5c2e90a8463c78
keith wong <keith wong>
2016-06-29 12:19:51 -0700

    quizapp html

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+}
+
+// Answer 5 questions
+
+// Show each question one at a time
+
+// Tell user each question's number
+
+// Show how many questions remain
+
+// Prevent user from skipping questions
+
+// Gives user some way to answer each question
+
+// Compare user's answer and the correct answer to determine score
+
+// Allow user to start new game after questions are answered
+
+// Use objects to represent questions and answers
