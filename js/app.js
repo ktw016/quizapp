6165f3b3ad2e9962159b03f3a9678f017e13752b
keith wong <keith wong>
2016-07-01 19:39:08 -0700

    start button

--- js/app.js
+++ js/app.js
@@ -1,195 +1,78 @@
-$(document).ready(function() {
+var questions = [{
+	question: "What is the capital of Australia?",
+	choices: ["Sydney", "Canberra", "Melbourne", "Adelaide"],
+	correct: 0,
+	},
+	{
+	question: "By area, what is the smallest country on the planet?",
+	answers: ["Vatican City", "Monaco", "Nauru", "Madagascar"],
+	correct: 0,
+	},
+	{
+	question: "What is the name of the tallest uninterrupted waterfall in the world?",
+	choices: ["Victoria Falls", "Angel Falls", "Niagara Falls", "Alamere Falls"],
+	correct: 1,
+	},
+	{
+	question: "What is the correct spelling of this landlocked Asian nation?",
+	choices: ["Kyrgyzstan", "Kurjistan", "Kergystan", "Kyigyzstan"],
+	correct: 0,
+	},
+	{
+	question: "What country is home to the tallest mountain in the world, Mount Everest?",
+	choices: ["Pakistan", "India", "Bhutan", "Nepal"],
+	correct: 3,
+}];
 
-	// Start Button
-	$('.start').on('click', function() {
-		console.log('start the game');
-		$('.intro').hide();
-		$('.hidden').removeClass();
-
-	});
-
-	$('.nextButton').on('click', function(){
-		console.log('start next question');
-
-	});
-
-	// Initialize game
-	game.question[question1, question2, question3, question4, question5]
-	game.currentQuestion = 0;
-	game.question[game.currentQuestion].writeToDom();
-
-	var game = Object.create(Game);
-	game.questions = []
-
-	// Questions
-	var question1 = Object.create(Question);
-	question1.prompt = "What is the capital of Australia?";
-	question1.a = "Sydney";
-	question1.b = "Canberra";
-	question1.c = "Melbourne";
-	question1.d = "Adelaide";
-	question1.correctAnswer = 'a';
-
-	var question2 = Object.create(Question);
-	question2.prompt = "By area, what is the smallest country on the planet?";
-	question2.a = "Vatican City";
-	question2.b = "Monaco";
-	question2.c = "Nauru";
-	question2.d = "Madagascar";
-	question2.correctAnswer = 'a';
-
-
-	var question3 = Object.create(Question);
-	question3.prompt = "What is the name of the tallest uninterrupted waterfall in the world?";
-	question3.a = "Victoria Falls";
-	question3.b = "Angel Falls";
-	question3.c = "Niagara Falls";
-	question3.d = "Alamere Falls";
-	question3.correctAnswer = 'b';
-
-
-	var question4 = Object.create(Question);
-	question4.prompt = "What is the correct spelling of this landlocked Asian nation?";
-	question4.a = "Kyrgyzstan";
-	question4.b = "Kurjistan";
-	question4.c = "Kergystan";
-	question4.d = "Kyigyzstan";
-	question4.correctAnswer = 'a';
-
-
-	var question5 = Object.create(Question);
-	question5.prompt = "What country is home to the tallest mountain in the world, Mount Everest?";
-	question5.a = "Pakistan";
-	question5.b = "India";
-	question5.c = "Bhutan";
-	question5.d = "Nepal";
-	question5.correctAnswer = 'd';
-
-
-	// Results
-	for(var i = 0; i < length; i++) {
-		var question = game.question[i];
-		question.writeToDom();
-		//wait for user selection
-		question.userAnswer = userSelection;
-			if (question.isCorrect()) {
-				$('.result').append("<h1>CORRECT!</h1>");
-				$('.result h1').hide();
-				$('.result h2').hide();
-			else {
-				$('.result').show();
-			}
-		}
-	}
-
-	// Start quiz
-	$('.intro').on('click', '.start', function() {
-		updateProgress();
-		currentQuestion++;
-		nextQuestion();
-	});
-
-	function updateProgress() {
-		if ()
-	}
-
-	function correctAnswer() {
-		if (userInput === correct)
-	}
-
-	function nextQuestion() {
-
-	}
-}
-
-var Question = {
-	prompt: null,
-	a: null,
-	b: null,
-	c: null,
-	d: null,
-	correctAnswer: null,
-	userAnswer: null,
-	isCorrect: function() {
-		return this.userAnswer === this.correctAnswer;
-	}
-	writeToDom: function() {
-		$('.something').append(this.prompt);
-	}
+var quiz = {
+	questions: question,
+	currentQuestion: 0,
+	score: 0,
+	nextQuestion: function() {
+		// Should first check to make sure we're not at the end
+		// Then increment this.currentQuestion
+		// Call a method to write to the DOM
+	},
+	writeToDOM: function() {
+		// use questions and currentQuestion to write to DOM
+		var question = this.questions[this.currentQuestion];
+		$('answer-div').append(question.question);
+		question.choices.forEach(function(choice, i) {
+			$('q-radio')[i].append(choice); 
+			// This may be the wrong way to work with radio in jQuery
+			// NOTE: http://stackoverflow.com/questions/596351/how-can-i-know-which-radio-button-is-selected-via-jquery
+			 
+		});
+	},
+	checkAnswer: function() {
+		// Will compare answer index to correct for current question
+		// If answer is correct increment this.score
+		var answer = $('q-radio').value();
+		// This may be the wrong way to work with radio in jQuery
+		if (answer === this.questions[this.currentQuestion].correct) 
+			//increment
+	},
 };
 
+$(document).ready(function() {
+	$('.start').on('click', function(e){
+		e.preventDefault();
+		$('#container').show('.questions');
+		$('.intro').hide('.start');
+	})
+	$('q-submit-button').on('click', function(e) {
+		e.preventDefault();
 
+		quiz.checkAnswer();
+		//Hide question div
+		//Show answer div
+	});
+	$('a-next-button').on('click', function(e) {
+		e.preventDefault();
 
+		quiz.nextQuestion();
+		// Hide answer div
+		// Show questions div
+	});
 
-// Answer 5 questions
-
-// Show each question one at a time
-
-// Tell user each question's number
-
-// Show how many questions remain
-
-// Prevent user from skipping questions
-
-// Gives user some way to answer each question
-
-// Compare user's answer and the correct answer to determine score
-
-// Allow user to start new game after questions are answered
-
-// Use objects to represent questions and answers
-
-
-// 	var quiz = [{
-// 		question: "What is the capital of Australia?",
-// 		choices: ["Sydney", "Canberra", "Melbourne", "Adelaide"],
-// 		correct: 0,
-// 		num:: 0
-// 		},
-// 		{
-// 		question: "By area, what is the smallest country on the planet?",
-// 		answers: ["Vatican City", "Monaco", "Nauru", "Madagascar"],
-// 		correct: 0,
-// 		num:: 1
-// 		},
-// 		{
-// 		question: "What is the name of the tallest uninterrupted waterfall in the world?",
-// 		choices: ["Victoria Falls", "Angel Falls", "Niagara Falls", "Alamere Falls"],
-// 		correct: 1,
-// 		num:: 2
-// 		},
-// 		{
-// 		question: "What is the correct spelling of this landlocked Asian nation?",
-// 		choices: ["Kyrgyzstan", "Kurjistan", "Kergystan", "Kyigyzstan"],
-// 		correct: 0,
-// 		num:: 3
-// 		},
-// 		{
-// 		question: "What country is home to the tallest mountain in the world, Mount Everest?",
-// 		choices: ["Pakistan", "India", "Bhutan", "Nepal"],
-// 		correct: 3,
-// 		num:: 4
-// 	}]
-
-// 	$('.intro').on('click', '.start', function() {
-// 		updateProgress();
-// 		currentQuestion++;
-// 		nextQuestion();
-// 	});
-// };
-
-// var currentQuestion = 0;
-
-
-// function updateProgress() {
-// 	if ()
-// }
-
-// function correctAnswer() {
-// 	if (userInput === correct)
-// }
-
-// function nextQuestion() {
-// 	if (currentQuestion < 5)
-
-// }
+});
